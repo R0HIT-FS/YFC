8d71b589fb2560e43eaac6c467f8245efdcfc68f
R0hit-KP <rohit.yallamelli@kraftpixel.com>
2026-04-01 21:25:32 +0530

    Added public link

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import UsersClient from "../components/UserClient";
 async function getUsers() {
-  const res = await fetch("http://localhost:3000/api/users", {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/users`, {
     cache: "no-store",
   });
   return res.json();
