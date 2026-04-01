ba11b8793c2cbf7ece301834bf7e2a3ef81761b9
R0hit-KP <rohit.yallamelli@kraftpixel.com>
2026-04-01 20:56:11 +0530

    types added again

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,19 +1,22 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI as string;
 
-if (!uri) {
-  throw new Error("Please add MONGODB_URI to .env.local");
-}
+const globalWithMongo = globalThis as typeof globalThis & {
+  _mongoClientPromise?: Promise<MongoClient>;
+};
 
-let client:MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-if (!global._mongoClientPromise) {
-  client = new MongoClient(uri);
-  global._mongoClientPromise = client.connect();
+if (process.env.NODE_ENV === "development") {
+  if (!globalWithMongo._mongoClientPromise) {
+    const client = new MongoClient(uri);
+    globalWithMongo._mongoClientPromise = client.connect();
+  }
+  clientPromise = globalWithMongo._mongoClientPromise;
+} else {
+  const client = new MongoClient(uri);
+  clientPromise = client.connect();
 }
-
-clientPromise = global._mongoClientPromise;
 
 export default clientPromise;
